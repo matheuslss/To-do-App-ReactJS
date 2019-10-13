8547f05aeb484206bf4bb50674c5dd9635f9ad2d
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-10-13 12:55:36 -0300

    add template Menu.jsx

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,9 +4,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Todo from '../components/todo/Todo'
 import About from '../components/about/About'
+import Menu from '../template/Menu'
 
 export default props => (
   <div className="container">
+    <Menu />
     <Todo />
     <About />
   </div>
