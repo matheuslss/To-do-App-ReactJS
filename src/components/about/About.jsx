49f606ce278482b45eec831b1d89246fb85ed2a1
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-10-13 13:06:04 -0300

    add template PageHeader.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from 'react'
+import PageHeader from '../../template/PageHeader'
 
 // import { Container } from './styles';
 
 export default props => (
   <div>
-    <h1>About</h1>
+    <PageHeader name='Sobre' small='Nós' />
+
+    <h2>Nossa história</h2>
+    <p>Lorem ipsum assum...</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum assum...</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum assum...</p>
+
   </div>
 )
