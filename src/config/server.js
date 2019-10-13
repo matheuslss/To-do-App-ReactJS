b14357cbd69055a8d7fe290466178dc1dc04dcf2
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-10-13 11:31:45 -0300

    add cors.js

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,11 @@ const port = 3333
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const cors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(port, () => 
   console.log(`Backend is running on port ${port}.`)
