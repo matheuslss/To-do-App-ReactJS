86fc89d2454f33179ad59d1f27787a232f5501d6
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-10-13 13:32:43 -0300

    refatorando template Grid

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from 'react'
+import Grid from '../../template/Grid'
 
 export default props => (
   <div role='form' className='todoForm'>
-    <div className="col-xs-12 col-sm-9 col-md-10">
-      <input 
-        id="description" 
-        className="form-control"
-        placeholder="Adicione uma tarefa"/>
-    </div>
-    <div className="col-xs-12 col-sm-3 col-md-2">
-      <button className="btn btn-primary">
-        <i className="fa fa-plus"></i>
-      </button>
-    </div>
+    <Grid cols="12 9 10 11">
+        <input 
+          id="description" 
+          className="form-control"
+          placeholder="Adicione uma tarefa"/>
+    </Grid>
+    <Grid cols="12 3 2 1">
+        <button className="btn btn-primary">
+          <i className="fa fa-plus"></i>
+        </button>
+    </Grid>  
   </div>
 )
