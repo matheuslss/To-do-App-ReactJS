e213e2bb8919f23d7a85b5a4825d3868541ed4d9
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2019-10-13 13:00:41 -0300

    add routes in app.jsx

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -2,6 +2,7 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import {Router, Route, Redirect, hashHistory } from 'react-router'
 import Todo from '../components/todo/Todo'
 import About from '../components/about/About'
 import Menu from '../template/Menu'
@@ -9,7 +10,10 @@ import Menu from '../template/Menu'
 export default props => (
   <div className="container">
     <Menu />
-    <Todo />
-    <About />
+    <Router history={ hashHistory }>
+      <Route path='/todos' component={ Todo } />
+      <Route path='/about' component={ About } />
+      <Redirect from='*' to='/todos' />
+    </Router>
   </div>
 )
